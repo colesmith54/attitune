ad9b336eb2ae90ae011eee2a5187b9723ef45e30
gowdajotsna <gowdajotsna@gmail.com>
2023-10-14 18:57:17 -0400

    playlist changes

--- web/src/components/Playlist.js
+++ web/src/components/Playlist.js
@@ -1,6 +1,6 @@
 'use client';
 import React, { useContext,useState ,useEffect} from 'react'
-
+import defaultdata from '../playlist.json';
 
 const Playlist = ({ playlistData }) =>{
 function millisecondsToMinutes(milliseconds) {
@@ -10,17 +10,7 @@ function millisecondsToMinutes(milliseconds) {
     return `${minutes}:${(seconds < 10 ? '0' : '')}${seconds}`;
     }
    
-    const playlist_data = playlistData ?? [{"_id": "265Anh9hGoozFigjUVLUeD", "album_image": "https://i.scdn.co/image/ab67616d0000b273280a44b1caee9f2109602c41",
-    "artist_names": ["Paolo Nutini"], "song_duration": 203653, "song_name": "New Shoes", 
-   "song_uri": "spotify:track:265Anh9hGoozFigjUVLUeD"},
-    {"_id": "51UtgWS4z1eMPuLQOzPtNH", "album_image": "https://i.scdn.co/image/ab67616d0000b27362d0614b5dffca14a302b427", "artist_names": ["311"], 
-    "song_duration": 211080, "song_name": "Amber", "song_uri": "spotify:track:51UtgWS4z1eMPuLQOzPtNH"}, {"_id": "312A8WfROSLvZbMDHBUPDp", 
-    "album_image": "https://i.scdn.co/image/ab67616d0000b2734bf409ede02e2a4069fd02a8", "artist_names": ["The Streets"],
-    "song_duration": 254266, "song_name": "Fit but You Know It", "song_uri": "spotify:track:312A8WfROSLvZbMDHBUPDp"},
-   
-    {"_id": "1FSWSs9CL01RCYxXtm08Rf", "album_image": "https://i.scdn.co/image/ab67616d0000b273ff2057b7343d2233451ff8e7", "artist_names": 
-    ["Olly Murs"], "song_duration": 202226, "song_name": "Dance with Me Tonight", "song_uri": "spotify:track:1FSWSs9CL01RCYxXtm08Rf"}];
-    
+    const playlist_data = playlistData ?? defaultdata
     return (
     <div className="overflow-x-auto">
     <table className="min-w-full bg-gradient-to-r from-gray-900 to-indigo-700">
