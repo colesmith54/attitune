17fa00421ca54242bf3add6716a1902d663a0988
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2023-10-14 01:20:13 -0400

    web button

---
+++ web/src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header>
+            <div className="relative flex items-center py-[2.125rem] shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] bg-gradient-to-r from-black via-violet-500 to-black">
+                <div className="absolute inset-x-0 bottom-0 h-px bg-slate-900/5"></div>
+
+            </div>
+        </header>
+    )
+}
