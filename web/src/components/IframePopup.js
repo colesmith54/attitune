dce286d3f89dd44c54f127a659950450416964f6
Cole Smith <colesmith5400@gmail.com>
2023-10-14 17:51:36 -0400

    add popup

---
+++ web/src/components/IframePopup.js
@@ -0,0 +1,41 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Iframe from 'react-iframe';
+import styles from '../app/styles/components/iframePopup.module.scss';
+
+
+function IframePopup({ url }) {
+  const [visible, setVisible] = useState(false);  // Visibility state inside the IframePopup
+
+  useEffect(() => {
+    if (visible) {
+      document.body.classList.add('iframe-active');
+    } else {
+      document.body.classList.remove('iframe-active');
+    }
+
+    return () => {
+      document.body.classList.remove('iframe-active');
+    };
+  }, [visible]);
+
+  return (
+    <>
+      <button onClick={() => setVisible(true)}>Show Iframe</button>
+
+      {visible && (
+        <div className={styles['iframe-overlay']}>
+          <button className={styles['close-button']} onClick={() => setVisible(false)}>X</button>
+          <Iframe url={url}
+                  width="640px"
+                  height="320px"
+                  className={styles['centered-iframe']}
+                  display="block"
+                  position="relative"/>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default IframePopup;
