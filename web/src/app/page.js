9585392c94c4e23cdff1163e9fc1ad254ca6389a
gowdajotsna <gowdajotsna@gmail.com>
2023-10-14 16:26:54 -0400

    playlist UI

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header"
 import SearchBox from "@/components/SearchBox"
 import MusicBar from "@/components/MusicBar"
 import styles from './styles/components/home.module.scss'
-
+import Playlist from "@/components/Playlist"
 export default function Home() {
   return (
     <div className={`min-h-screen`}>
@@ -11,7 +11,9 @@ export default function Home() {
      <main className="flex min-h-screen flex-col mt-5 px-24">
       <SearchBox />
           <div className="songCover"></div>
-          <div className="playlist"></div>
+          <div className="col-md-6">
+          <div className="playlist"><Playlist/></div>
+          </div>
           <div className="mt-10">
         <div className={`${styles.musicBar} w-full`}>
             <MusicBar />
--- web/src/components/Playlist.js
+++ web/src/components/Playlist.js
@@ -1,49 +1,56 @@
+'use client';
 import React, { useContext,useState ,useEffect} from 'react'
-import {Link,useNavigate, useLocation} from "react-router-dom"
 import { Card } from 'react-bootstrap'; 
 
 
 
 const Playlist = () =>{
-const playlist_data=[
-    {
-      "album_image": "image_url_1",
-      "artist_names": "Artist 1",
-      "song_duration": "3:45",
-      "song_name": "Song 1",
-      "song_uri": "spotify:track:12345"
-    },
-    {
-      "album_image": "image_url_2",
-      "artist_names": "Artist 2",
-      "song_duration": "4:15",
-      "song_name": "Song 2",
-      "song_uri": "spotify:track:67890"
-    },
-  ]
+const playlist_data=[{"_id": "265Anh9hGoozFigjUVLUeD", "album_image": "https://i.scdn.co/image/ab67616d0000b273280a44b1caee9f2109602c41",
+    "artist_names": ["Paolo Nutini"], "song_duration": 203653, "song_name": "New Shoes", 
+   "song_uri": "spotify:track:265Anh9hGoozFigjUVLUeD"},
+    {"_id": "51UtgWS4z1eMPuLQOzPtNH", "album_image": "https://i.scdn.co/image/ab67616d0000b27362d0614b5dffca14a302b427", "artist_names": ["311"], 
+    "song_duration": 211080, "song_name": "Amber", "song_uri": "spotify:track:51UtgWS4z1eMPuLQOzPtNH"}, {"_id": "312A8WfROSLvZbMDHBUPDp", 
+    "album_image": "https://i.scdn.co/image/ab67616d0000b2734bf409ede02e2a4069fd02a8", "artist_names": ["The Streets"],
+    "song_duration": 254266, "song_name": "Fit but You Know It", "song_uri": "spotify:track:312A8WfROSLvZbMDHBUPDp"},
+   
+    {"_id": "1FSWSs9CL01RCYxXtm08Rf", "album_image": "https://i.scdn.co/image/ab67616d0000b273ff2057b7343d2233451ff8e7", "artist_names": 
+    ["Olly Murs"], "song_duration": 202226, "song_name": "Dance with Me Tonight", "song_uri": "spotify:track:1FSWSs9CL01RCYxXtm08Rf"}];
+
 
 
     return (
-       <div>
-       {Object.keys(playlist_data).length > 0 && (
-        <div className="playlist">
-          {playlist_data.items.map((item, index) => {
-            return (
-              <React.Fragment key={index}>
-                <Card style={{ width: '18rem' }}>
-               
-                  <Card.Body>
-                    <Card.Title>{item.song_name}</Card.Title>
-                    <Card.Text>
-                      <small>By {item.artist_names}</small>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </React.Fragment>
-            );
-          })}
-        </div>
-      )}
+       <div classname="container">
+        <div classname="row">
+            <div className="col-md-6">
+            {/* This is the empty column */}
+            </div>
+            {Object.keys(playlist_data).length > 0 && (
+                <div className="playlist">
+                {playlist_data.map((item, index) => {
+                    return (
+                    <React.Fragment key={index}>
+                        <Card class="bg-gradient-to-r from-indigo-500 ..." style={{ width: '18rem' }}>
+                        <Card.Body>
+                        <div className="flex items-center space-x-2"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-music-note" viewBox="0 0 16 16">
+                            <path d="M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z"/>
+                            <path fill-rule="evenodd" d="M9 3v10H8V3h1z"/>
+                            <path d="M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z"/>
+                            </svg>
+                            <Card.Title class="text-lg font-semibold text-white font-helvetica">{item.song_name}</Card.Title>
+                        </div>
+                        <Card.Text>
+                            <small className="mt-3 ml-6" style={{ color: 'white' }}>By {item.artist_names.slice(0, 2).join(', ')}</small>
+                        </Card.Text>
+                            
+                        </Card.Body>
+                        </Card>
+                    </React.Fragment>
+                    );
+                })}
+                </div>
+            )}
+      </div>
     </div>
     );
 };
