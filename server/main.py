b3793e6d87f9d846f120adf082db15edeec02a18
gowdajotsna <gowdajotsna@gmail.com>
2023-10-14 14:43:07 -0400

    dummyplaylist

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from apiParser import apiParser  
 from spotify import fetch_songs
-
+import json
 app=Flask(__name__)
 
 CORS(app)
@@ -12,8 +12,8 @@ def analyze_sentiment():
     search_query = request.form.get('search_query', '')
     open_ai_scores = apiParser.testApi(search_query)
     results = fetch_songs(*open_ai_scores.values())
-
     print([song["song_name"] for song in results])
+   
     return jsonify(results)
 
 if __name__=='__main__':
