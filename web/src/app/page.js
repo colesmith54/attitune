9eaa26cbf06c1e8219c9453b74b4de88641dcb85
Cole Smith <colesmith5400@gmail.com>
2023-10-14 17:54:43 -0400

    add back iframe

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -4,7 +4,7 @@ import Header from "@/components/Header"
 import SearchBox from "@/components/SearchBox"
 import styles from './styles/components/home.module.scss'
 import Playlist from "@/components/Playlist"
-// import IframePopup from "@/components/IframePopup";
+import IframePopup from "@/components/IframePopup";
 
 export default function Home() {
 
@@ -77,9 +77,9 @@ export default function Home() {
           <div className="playlist"><Playlist playlistData={playlistData}/></div>
         </div>
 
-        {/* <IframePopup 
+        { <IframePopup 
           url="https://open.spotify.com/embed/track/2RlgNHKcydI9sayD2Df2xp?utm_source=generator"
-        /> */}
+        />}
       </main>
     </div>
   )
