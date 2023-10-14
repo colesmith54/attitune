b7828cb53d0f71794fe28a89e77072257ed4c04f
Cole Smith <colesmith5400@gmail.com>
2023-10-14 17:34:08 -0400

    popup

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,34 +1,23 @@
-//import Image from 'next/image'
-import Header from "@/components/Header"
-import SearchBox from "@/components/SearchBox"
-import MusicBar from "@/components/MusicBar"
-import styles from './styles/components/home.module.scss'
-import Playlist from "@/components/Playlist"
-import Iframe from 'react-iframe'
+import Header from "@/components/Header";
+import SearchBox from "@/components/SearchBox";
+import styles from './styles/components/home.module.scss';
+import Playlist from "@/components/Playlist";
+import IframePopup from "@/components/IframePopup";
 
 export default function Home() {
   return (
     <div className={`min-h-screen`}>
-     <Header />
-     <main className="flex min-h-screen flex-col mt-5 px-24">
-      <SearchBox />
-          <div className="col-md-6">
-            <div className="playlist"><Playlist/></div>
-          </div>
-          <div className="songCover">
-          <Iframe url="https://open.spotify.com/embed/track/2RlgNHKcydI9sayD2Df2xp?utm_source=generator"
-                  width="640px"
-                  height="320px"
-                  id=""
-                  className=""
-                  display="block"
-                  position="relative"/>
-          </div>
-          <div className="mt-10">
-          <div className={`${styles.musicBar} w-full`}>
-          </div>
+      <Header />
+      <main className="flex min-h-screen flex-col mt-5 px-24">
+        <SearchBox />
+        <div className="col-md-6">
+          <div className="playlist"><Playlist/></div>
         </div>
-    </main>
+
+        <IframePopup 
+          url="https://open.spotify.com/embed/track/2RlgNHKcydI9sayD2Df2xp?utm_source=generator"
+        />
+      </main>
     </div>
   )
 }
