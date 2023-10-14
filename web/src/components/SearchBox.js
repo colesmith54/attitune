3efaf5f26bc3ca04725753d02f7f5fee23986c1d
Cole Smith <colesmith5400@gmail.com>
2023-10-14 16:50:55 -0400

    local storage

--- web/src/components/SearchBox.js
+++ web/src/components/SearchBox.js
@@ -1,37 +1,81 @@
 'use client';
-import { useState } from 'react';
-import styles from '../app/styles/components/searchBox.module.scss'
+import { useState, useEffect } from 'react';
+import styles from '../app/styles/components/searchBox.module.scss';
 
 const SearchBox = (props) => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [loadedFromStorage, setLoadedFromStorage] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const formData = new FormData(form);
-        const response = fetch("http://127.0.0.1:5000/api/analyze_sentiment",{
-            method: form.method, // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-store", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
+    const handleSubmit = (data) => {
+        const formData = new FormData();
+        formData.append('search_query', data);
+        
+        fetch("http://127.0.0.1:5000/api/analyze_sentiment", {
+            method: 'post',
+            mode: "cors",
+            cache: "no-store",
+            credentials: "same-origin",
             body: formData
         });
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
-    }
-      
-    return(
+
+        localStorage.setItem('search_query', data); // Use localStorage instead
+    };
+
+    useEffect(() => {
+        const storedQuery = localStorage.getItem('search_query'); // Use localStorage instead
+        if (storedQuery) {
+            setSearchQuery(storedQuery);
+            setLoadedFromStorage(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (loadedFromStorage) {
+            handleSubmit(searchQuery);
+            setLoadedFromStorage(false); 
+        }
+    }, [loadedFromStorage]);
+
+    useEffect(() => {
+        const handleBeforeUnload = (e) => {
+            const windowsCount = window.open(null, '_self').length;
+            if (windowsCount === 1) { // If only one window/tab is open
+                localStorage.removeItem('search_query'); // Clear the stored data
+            }
+        };
+
+        window.addEventListener('beforeunload', handleBeforeUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, []);
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        handleSubmit(searchQuery);  
+    };
+
+    return (
         <div>
-            <form method="post" onSubmit={handleSubmit}>
-            <label htmlFor="search" className="block text-large font-large leading-6 text-white">How is your mood?</label>
-            <div className="mt-2">
-              <textarea id="search" name="search_query" rows="2" className={`${styles.searchBox} border-white focus:border-[#7c3aed] rounded`}></textarea>
-            </div>
-            <button type="submit" className="bg-violet-500 hover:bg-indigo-400 text-white font-semibold px-6 py-3 rounded-md">
-                Show Recommendations
-            </button>
+            <form method="post" onSubmit={handleFormSubmit}>
+                <label htmlFor="search" className="block text-large font-large leading-6 text-white">How is your mood?</label>
+                <div className="mt-2">
+                    <textarea
+                        id="search"
+                        name="search_query"
+                        rows="2"
+                        className={`${styles.searchBox} border-white focus:border-[#7c3aed] rounded`}
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    ></textarea>
+                </div>
+                <button type="submit" className="bg-violet-500 hover:bg-indigo-400 text-white font-semibold px-6 py-3 rounded-md">
+                    Show Recommendations
+                </button>
             </form>
         </div>
     )
-}
+};
 
-export default SearchBox
+export default SearchBox;
