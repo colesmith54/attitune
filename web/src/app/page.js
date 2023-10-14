25ec59d3cea8396e2774b02561c83001a2b988a8
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2023-10-14 16:52:24 -0400

    iframe

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -4,19 +4,28 @@ import SearchBox from "@/components/SearchBox"
 import MusicBar from "@/components/MusicBar"
 import styles from './styles/components/home.module.scss'
 import Playlist from "@/components/Playlist"
+import Iframe from 'react-iframe'
+
 export default function Home() {
   return (
     <div className={`min-h-screen`}>
      <Header />
      <main className="flex min-h-screen flex-col mt-5 px-24">
       <SearchBox />
-          <div className="songCover"></div>
           <div className="col-md-6">
-          <div className="playlist"><Playlist/></div>
+            <div className="playlist"><Playlist/></div>
+          </div>
+          <div className="songCover">
+          <Iframe url="https://open.spotify.com/embed/track/2RlgNHKcydI9sayD2Df2xp?utm_source=generator"
+                  width="640px"
+                  height="320px"
+                  id=""
+                  className=""
+                  display="block"
+                  position="relative"/>
           </div>
           <div className="mt-10">
-        <div className={`${styles.musicBar} w-full`}>
-            <MusicBar />
+          <div className={`${styles.musicBar} w-full`}>
           </div>
         </div>
     </main>
--- web/src/components/MusicBar.js
+++ web/src/components/MusicBar.js
@@ -1,10 +1,81 @@
+'use client';
+
 import Volume from '../app/svgs/volume.svg'
 import LeftSeek from '../app/svgs/leftSeek.svg'
 import RightSeek from '../app/svgs/rightSeek.svg'
 import Play from '../app/svgs/play.svg'
+import Pause from '../app/svgs/pause.svg'
 import styles from '../app/styles/components/music.module.scss'
+import { useEffect, useState } from 'react'
 
 const MusicBar = () => {
+    const [playerInstance, setPlayerInstance] = useState(null)
+    const [clientToken, setClientToken] = useState(null)
+    const [deviceId, setDeviceId] = useState(null)
+
+    const generateSpotifyToken = async () => {
+        const url = "https://accounts.spotify.com/api/token"
+        const headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        const payload = {
+            'grant_type': 'authorization_code',
+            'client_id': process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+            'client_secret': process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+        }
+    
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: "cors", // no-cors, *cors, same-origin
+            headers: headers,
+            body: new URLSearchParams(payload)
+        })
+        
+        const res = await response.json()
+        console.log(res)
+        return res?.access_token
+    }
+
+    useEffect(()=>{
+
+        const fetchPlayerInstance = async () => {
+            const t = await generateSpotifyToken()
+            setClientToken(t)
+            if(window.Spotify){
+                const player = new window.Spotify.Player({
+                    name: 'Attitune',
+                    getOAuthToken: cb => { cb(t); }
+                });
+                console.log("window.Spotify:: ", window.Spotify)
+                console.log("player:: ", player)
+                setPlayerInstance(player)
+                
+                player.addListener('ready', ({ device_id }) => {
+                    console.log('Ready with Device ID', device_id);
+                });
+        
+                player.addListener('not_ready', ({ device_id }) => {
+                    console.log('Device ID has gone offline', device_id);
+                });
+        
+                player.connect();
+                
+            }
+        }
+   
+
+    if(!clientToken){
+        fetchPlayerInstance()
+        // make sure to catch any error
+        .catch("Error: ", console.error);
+    }
+   
+        
+    },[playerInstance, setPlayerInstance])
+
+
+
     return (
         <div className="relative z-10 w-full">
             <div className={`${styles.musicbar} flex w-100 bg-black border-t-2 border-slate-500 shadow-white/5 ring-1 ring-slate-700/10`}>
@@ -14,6 +85,9 @@ const MusicBar = () => {
                     </div>
                     <div className='h-10 w-10 flex-none'>
                     <Play />
+                    </div>
+                    <div className='h-8 w-8 flex-none'>
+                    <Pause />
                     </div>
                     <div className='h-6 w-6 flex-none'>
                     <RightSeek />
